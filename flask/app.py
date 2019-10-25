6343d5e305c8bc8bed1c051499c52dc4af0fe72e
Dmitry <inyutin.da@gmail.com>
2019-10-26 01:28:08 +0300

    merge backend

---
+++ flask/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/api/getAll')
+def get_all():
+    return jsonify([
+        {
+            "name": "Кудрявцев Николай Николаевич",
+            "id": 21583,
+            "info": "Ректор ФГАОУ ВО Московский физико-технический институт (государственный университет)"
+        },
+        {
+            "name": "Путин Владимир Владимирович",
+            "id": 582,
+            "info": "Президент Российской Федерации"
+        }
+    ])
+
+@app.route('/api/get/<int:person_id>')
+def get_one(person_id):
+    return jsonify(
+        {
+            "name": "Кудрявцев Николай Николаевич",
+            "salary": 20531276,
+            "otherSalaries": [
+                {
+                    "name": "Медицинская сестра",
+                    "salary": 28200
+                },
+                {
+                    "name": "Учитель",
+                    "salary": 92944
+                }
+            ],
+        }
+    )
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
---
+++ readme.md
@@ -0,0 +1,4 @@
+Остановить всё - `docker-compose down`
+Пересобратьвсё - `docker-compose build`
+Запустить всё (в терминале) - `docker-compose up`
+Запустить всё (в detached-режиме) `docker-compose up -d`
