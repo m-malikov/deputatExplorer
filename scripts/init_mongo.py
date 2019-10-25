11a509a43dbbd8348be628a2068652a540104327
Dmitry <inyutin.da@gmail.com>
2019-10-26 01:21:48 +0300

    init db

---
+++ scripts/init_mongo.py
@@ -0,0 +1,35 @@
+import json
+import pymongo
+from pymongo.bulk import BulkWriteError
+from collections import defaultdict
+
+DATA_FILE_NAME = 'declarations.json'
+
+client = pymongo.MongoClient("176.99.11.79:27017", username="root", password="example")
+db = client.declarations
+print("Server Status: ", db.command("serverStatus"))
+
+with open(DATA_FILE_NAME) as json_file:
+    data = json.load(json_file)
+
+print("Количество строк в данных:", len(data))
+
+data_to_db = defaultdict(lambda: [])
+for person in data:
+    _id = person['main']['person']['id']
+    data_to_db[_id].append(person)
+
+del data
+
+query = []
+for key, value in data_to_db.items():
+    query.append({
+        "_id": key,
+        "declarations": value
+    })
+
+try:
+    db.persons.insert_many(query)
+except BulkWriteError as e:
+    print("Error")
+    print(e.details)
